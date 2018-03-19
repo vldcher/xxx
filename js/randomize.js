ef599032f3a7e1535faf935c0836698462594ce6
Valentyna Tuparieva <valentyna_tuparieva@epam.com>
2018-03-19 12:05:12 +0200

    .

--- js/randomize.js
+++
@@ -1,9 +0,0 @@
-function randomize(a, max, min) {
-    let matrix = [];
-    for (let i = 0; i < a; i++){
-        for (let j = 0; j < a; j++) {
-            matrix[i][j] =  Math.random() * (max - min) + min;
-        }
-    }
-    return matrix;
-}

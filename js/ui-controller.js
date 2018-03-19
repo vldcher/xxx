c1c941cfb9f55641b4d51f20dab87ddde0c9e4e8
yarik.lysak <zhbrkscfr@gmail.com>
2018-03-19 17:06:36 +0200

    debuging

--- js/ui-controller.js
+++ js/ui-controller.js
@@ -1,46 +1,48 @@
-import {Engine} from './engine';
-import {UI} from './ui';
+import { Engine } from './engine';
+import { UI } from './ui';
 
 class Controller {
-    constructor(UI, engine) {
-        this.UI = UI;
-        this.Engine = engine;
-        this.totalAmount = 0;
-        this.level = 0;
-        this.n = engine.clicks;
+  constructor(UI, engine) {
+    this.UI = UI;
+    this.Engine = engine;
+    this.totalAmount = 0;
+    this.level = 0;
+    this.n = engine.clicks;
+  }
+
+  init() {
+    let matrix = this.Engine.getMatrix();
+    this.UI.showEntireMatrix(matrix);
+  }
+
+  openCellHandler(i, j) {
+    let isOpen = UI.isMatrixOpen; //default - false
+    if (isOpen) {
+      return;
     }
 
-    init() {
-        let matrix = this.Engine.getMatrix();
-        this.UI.showEntireMatrix(matrix);
+    this.n--; //count user's possible click
+
+    if (this.n < 1) {
+      UI.levelFailed();
     }
-
-    openCellHandler(i, j) {
-        let isOpen = UI.isMatrixOpen; //default - false
-        if (isOpen) {
-            return;
-        }
-
-        this.n--; //count user's possible click
-        if (this.n < 1) {
-            UI.levelFailed();
-        }
-        if (this.totalAmount > this.Engine.getSuccessRate()) {
-            UI.levelSuccess();
-            this.totalAmount = 0;
-            Engine.startNextLevel();
-            let matrix = Engine.getMatrix();
-            UI.showEntireMatrix(matrix);
-        } else{
-            let matrix = this.Engine.getMatrix();
-            let amount = matrix.getValue(i,j);
-            this.totalAmount += amount;
-            UI.openCell(i, j, amount, this.totalAmount); //open cell
-            UI.showEntireMatrix(matrix);
-        }
-
+    if (this.totalAmount > this.Engine.getSuccessRate()) {
+      UI.levelSuccess();
+      this.totalAmount = 0;
+      Engine.startNextLevel();
+      let matrix = Engine.getMatrix();
+      UI.showEntireMatrix(matrix);
+    } else {
+      let matrix = this.Engine.getMatrix();
+      let amount = matrix.getValue(i, j);
+      this.totalAmount += amount;
+      UI.openCell(i, j, amount, this.totalAmount); //open cell
+      UI.showEntireMatrix(matrix);
     }
+  }
 }
 
 let c = new Controller(new UI(), new Engine());
-document.querySelector('.startButton').addEventListener('click', ()=>c.init());
+document
+  .querySelector('.startButton')
+  .addEventListener('click', () => c.init());
--- js/ui.js
+++ js/ui.js
@@ -1,53 +1,57 @@
 export class UI {
+  constructor() {
+    this.isMatrixOpen = false;
+    this.TIME = 2000;
+    this.startButton = document.querySelector('.startButton');
+    this.nextButton = document.querySelector('.nextButton');
+    this.matrixCells = Array.from(document.querySelectorAll('.cell-item'));
+  }
 
-	constructor() {
-		this.isMatrixOpen = false;
-		this.TIME = 2000;
-		this.startButton = document.querySelector('.startButton');
-		this.nextButton = document.querySelector('.nextButton');
-		this.matrixCells = Array.from(document.querySelectorAll('.cell-item'));
-	}
+  levelStart(level) {
+    // get current level
+    this.startButton.disabled = true;
+    // this.nextButton.style.display = none;
+    alert('start new level');
+  }
 
-	levelStart(level) { // get current level
-		this.startButton.disabled = true;
-		// this.nextButton.style.display = none;
-		alert('start new level');
-	}
+  levelSuccess() {
+    alert('go to the nex level');
+    this.nextButton.style.display = block;
+  }
 
-	levelSuccess() {
-		alert('go to the nex level');
-		this.nextButton.style.display = block;
-	}
+  levelFailed() {
+    alert('you are failed, try again');
+    this.startButton.disabled = false;
+    this.nextButton.style.display = none;
+  }
 
-	levelFailed() {
-		alert('you are failed, try again');
-		this.startButton.disabled = false;
-		this.nextButton.style.display = 'none';
-	}
+  openCell(i, j, amount, totalAmount) {
+    // i, j - box address, amount - gold in box, totalAmount - sum of golg from open boxes
+  }
 
-	openCell(i, j, amount, totalAmount) { // i, j - box address, amount - gold in box, totalAmount - sum of golg from open boxes
+  onClick(e) {
+    // console.log(e.target.dataset);
+    e.target.classList.add('rotateRing');
+  }
 
-	}
+  showEntireMatrix(matrix) {
+    //show matrix at start and close it for play
+    try {
+      this.matrixCells.forEach((element, index) => {
+        element.innerHTML = `<p class="gold"> ${matrix.getValue(
+          Math.floor(index / matrix.size),
+          index % matrix.size
+        )} </p>`;
+        element.addEventListener('click', e => this.onClick(e));
+      });
+    } catch (e) {}
+    this.isMatrixOpen = true;
 
-	onClick(e) {
-		console.log(e.target.dataset)
-		e.target.classList += 'rotateRing';
-	}
-
-	showEntireMatrix(matrix) { //show matrix at start and close it for play
-		try {
-            this.matrixCells.forEach((element, index) => {
-
-                element.innerHTML = `<p class="gold"> ${matrix.getValue(Math.floor(index / matrix.size), index % matrix.size)} </p>`;
-            	element.addEventListener('click',(e) => this.onClick(e))
-            });
-        }
-        catch (e) {}
-		this.isMatrixOpen = true;
-		setTimeout(() => {
-			this.isMatrixOpen = false;
-			document.querySelector('.gold').style.display = none;
-			}, this.TIME);
-	}
-
+    setTimeout(() => {
+      this.isMatrixOpen = false;
+      document
+        .querySelectorAll('.gold')
+        .forEach(item => (item.style.display = 'none'));
+    }, this.TIME);
+  }
 }

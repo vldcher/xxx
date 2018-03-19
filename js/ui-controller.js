350bb46144b9ded427aac1782add13468bb27720
Tatyana Marchenko <Tatyana_Marchenko@epam.com>
2018-03-19 11:58:57 +0200

    controllerUI

--- js/ui-controller.js
+++ js/ui-controller.js
@@ -1,44 +1,29 @@
-
-let N = 10;
-let totalAmount = 0;
-
-class Engine {
-
-    getMatrix(){
-        let mass = [[1,2,3],[4,5,6],[7,8,9]];
-       return mass
-    }
-}
-
 class Controller {
     constructor(){
-        // let UI =  new UI();
+        let UI =  new UI();
         let EngineUI = new Engine();
 
     }
 
     init() {
-        // let start = UI.levelStart(matrix);
+        let start = UI.levelStart(matrix);
     }
 
     openCellHandler(i,j){
-        let isOpen = UI.matrixOpen();
+        let isOpen = UI.isMatrixOpen;
         if(isOpen){
             return;
         }
-        N--;
+        N--;            //count user's possible click
         if (N==0){
             UI.levelFailed()
         }
         let matrix = EngineUI.getMatrix();
         let amount = matrix[i,j];
         totalAmount+=amount;
-        UI.openCell(i, j, amount, totalAmount);
+        UI.openCell(i, j, amount, totalAmount);     //open cell
 
     }
 }
 
-// const EngineK = new Engine();
-// console.log(EngineK.getMatrix())
-const ControllerUI = new Controller();
-ControllerUI.init();
+export Controller;
